cb16cd794b6320304a923558bba07c49a5c1ca15
Juan Carlos Diaz <judiazp2p@gmail.com>
2021-11-07 19:49:19 -0300

    The Anchors component was created whose responsibility is to render a list of links

---
+++ components/organisms/Anchors.jsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import PropTypes from 'prop-types';
+
+import {anchors, tagList} from '../../styles/Anchors.module.css'
+
+export default function Anchors({array}) {
+        
+    return (
+        <div className={tagList}>
+            {
+                array.map( item => (
+                    <Link 
+                        href={`#/tema/${item}`}
+                        key={item}
+                    >
+                        <a className={anchors}>
+                            ·  {item}
+                        </a>
+                    </Link>
+                ))
+            }
+        </div>
+    )
+}
+
+Anchors.propTypes = {
+    array: PropTypes.array
+}
